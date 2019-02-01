5b36d87378059b1c3633b72fd247b0d509d28ecc
Your Name <you@example.com>
2019-01-31 20:31:33 -0500

    added ExpressJS support

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET products page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+/* GET services page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Content page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Content' });
+});
 module.exports = router;
